3a9d44147137f15774eb7adb8914b9a2b627602f
Nahuel Herrera <naherrera09@gmail.com>
2022-03-30 20:51:26 -0300

    seguims

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {ShoppingCartIcon} from '@heroicons/react/solid'
+const CartWidget = () => {
+  return (
+    <div>
+     <ShoppingCartIcon/>
+    </div>
+  )
+}
+
+export default CartWidget
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import CartWidget from './CartWidget'
 
 const Navbar = () => {
   return (
     <>
-        <nav className='bg-neutral-400 container mx-auto px-6 py-3'>
+        <nav className='bg-neutral-400 h-[80px] mx-auto px-6 py-3 flex justify-between items-center'>
            
-            <div id='logo' className='flex'><span>SUPPORT</span></div>  
+
+            <div id='logo' className=''>
+                <span>SUPPORT</span>
+            </div>  
            
-                <ul className='flex justify-center'>
+                <ul className='flex'>
                     <li className='px-2'> <a href="">Nosotros</a></li>
                     <li className='px-2'><a href="">Categorías</a> </li>
                     <li className='px-2 '><a href="">Envíos</a></li>
+                    <CartWidget/>
                 </ul>
         
         </nav>
